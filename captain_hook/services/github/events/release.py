ca362705ae1f681ed5a081da557c62aa4341988a
brantje <brantje@gmail.com>
2017-02-26 21:57:28 +0100

    Implement release event

---
+++ captain_hook/services/github/events/release.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from ...base.events import BaseEvent
+
+
+class ReleaseEvent(BaseEvent):
+    def process(self):
+        params = {
+            'username': self.body['sender']['login'],
+            'user_link': self.body['sender']['html_url'],
+            'tag': self.body['release']['tag_name'],
+            'tag_link': self.body['release']['html_url'],
+            'repository_name': self.body['repository']['full_name'],
+            'repository_link': self.body['repository']['html_url'],
+        }
+        message = False
+        if self.body['action'] == 'published':
+            message = "🚀 [{username}]({user_link}) added tag [{tag}]({tag_link}) to [{repository_name}]({repository_link})"
+            message = message.format(**params)
+
+        return {"telegram": str(message)}
