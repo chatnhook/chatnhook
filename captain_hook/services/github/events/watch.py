e2a9a9d91affad1857c4d7038057bbc2597a050d
brantje <brantje@gmail.com>
2017-02-26 21:57:12 +0100

    Implement watch event

---
+++ captain_hook/services/github/events/watch.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from ...base.events import BaseEvent
+
+
+class WatchEvent(BaseEvent):
+    def process(self):
+        user_link = self.body['sender']['html_url'].replace('https://github.com/', '')
+        repo_link = self.body['repository']['html_url'].replace('https://github.com/', '')
+
+        params = {
+            'username': self.body['sender']['login'],
+            'user_link': self.body['sender']['html_url'],
+            'repository_name': self.body['repository']['full_name'],
+            'repository_link': self.body['repository']['html_url'],
+        }
+        message = False
+        if self.body['action'] == 'started':
+            message = "❤ [{username}]({user_link}) starred [{repository_name}]({repository_link})"
+            message = message.format(**params)
+
+        return {"telegram": str(message)}
