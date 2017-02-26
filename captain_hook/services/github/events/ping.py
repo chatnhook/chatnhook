767c13598ebd30408d640651ae1f2824fadbec91
brantje <brantje@gmail.com>
2017-02-26 21:22:17 +0100

    Implement ping event

---
+++ captain_hook/services/github/events/ping.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from ...base.events import BaseEvent
+
+
+class PingEvent(BaseEvent):
+
+    def process(self):
+        params = {
+            'repo': self.body['repository']['full_name']
+        }
+
+        message = "Webhook works for: {repo}".format(**params)
+
+        return {"telegram": str(message)}
