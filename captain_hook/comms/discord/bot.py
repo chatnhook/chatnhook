64c69af4073522af00d326b32638a5bcc0384307
brantje <brantje@gmail.com>
2017-02-27 19:30:46 +0100

    Add discord comm

---
+++ captain_hook/comms/discord/bot.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from ..base.base_comm import BaseComm
+
+from .discordWebhooks import DiscordWebhook
+
+
+class DiscordComm(BaseComm):
+
+    def setup(self):
+        self.discord_bot = DiscordWebhook(url=self.config["hook_url"])
+
+    def communicate(self, message):
+        if not message:
+            return None
+        message = 'test'
+        self.discord_bot.notify(text=message, username=self.config['bot_name'])
+
+        #     sendMessage(
+        #     self.config["channel"],
+        #     message,
+        #     parse_mode=telegram.ParseMode.MARKDOWN
+        # )
