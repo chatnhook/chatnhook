362711a57473c1f57cc8d5327e1ddb98de1f9c67
brantje <brantje@gmail.com>
2017-02-27 15:24:25 +0100

    Add mattermost comm

--- captain_hook/comms/telegram/bot.py
+++ captain_hook/comms/telegram/bot.py
@@ -6,12 +6,12 @@ from ..base.base_comm import BaseComm
 class TelegramComm(BaseComm):
 
     def setup(self):
-        self.bot = telegram.Bot(self.config["token"])
+        self.telegram_bot = telegram.Bot(self.config["token"])
 
     def communicate(self, message):
         if not message:
             return None
-        self.bot.sendMessage(
+        self.telegram_bot.sendMessage(
             self.config["channel"],
             message,
             parse_mode=telegram.ParseMode.MARKDOWN
