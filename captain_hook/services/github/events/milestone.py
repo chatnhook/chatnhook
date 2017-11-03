6d016ccfbcf3b3c424ac851f2edacc79b1100dc6
brantje <brantje@gmail.com>
2017-11-03 19:25:20 +0100

    Add some github events. With empty messages...

---
+++ captain_hook/services/github/events/milestone.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from . import GithubEvent
+
+
+class MilestoneEvent(GithubEvent):
+
+    def process(self, request, body):
+        return {"default": ''}
+
+    def get_redirect(self, request, event, params):
+        redirect = {
+            'meta_title': '',
+            'meta_summary': '',
+            'poster_image': '',
+            'redirect': 'https://github.com/' + params,
+            'status_code': 404
+        }
+        return redirect
