75c6cb6cfe31c24aeb4ebfc17aa702eefa78bfe5
brantje <brantje@gmail.com>
2017-11-08 22:44:53 +0100

    Small fixes

--- captain_hook/stats/stats.py
+++ captain_hook/stats/stats.py
@@ -1,37 +1,34 @@
-from __future__ import absolute_import
-
-
 class BotStats():
-	stats = {
-		'messages': {},
-		'webhooks': {},
-		'redirects': {}
-	}
+    stats = {
+        'messages': {},
+        'webhooks': {},
+        'redirects': {}
+    }
 
-	def count_message(self, service):
-		if service in self.stats['messages']:
-			self.stats['messages'][service] = self.stats['messages'][service] + 1
+    def count_message(self, service):
+        if service in self.stats['messages']:
+            self.stats['messages'][service] = self.stats['messages'][service] + 1
 
-		if service not in self.stats['messages']:
-			self.stats['messages'][service] = 1
+        if service not in self.stats['messages']:
+            self.stats['messages'][service] = 1
 
-	def count_webhook(self, service, event=''):
-		if service in self.stats['webhooks']:
-			if event in self.stats['webhooks'][service]:
-				self.stats['webhooks'][service][event] = self.stats['webhooks'][service][event] + 1
+    def count_webhook(self, service, event=''):
+        if service in self.stats['webhooks']:
+            if event in self.stats['webhooks'][service]:
+                self.stats['webhooks'][service][event] += 1
 
-		if service not in self.stats['webhooks']:
-			self.stats['webhooks'][service] = {}
+        if service not in self.stats['webhooks']:
+            self.stats['webhooks'][service] = {}
 
-		if event not in self.stats['webhooks'][service]:
-			self.stats['webhooks'][service][event] = 1
+        if event not in self.stats['webhooks'][service]:
+            self.stats['webhooks'][service][event] = 1
 
-	def count_redirect(self, service):
-		if service in self.stats['redirects']:
-			self.stats['redirects'][service] = self.stats['redirects'][service] + 1
+    def count_redirect(self, service):
+        if service in self.stats['redirects']:
+            self.stats['redirects'][service] += 1
 
-		if service not in self.stats['redirects']:
-			self.stats['redirects'][service] = 1
+        if service not in self.stats['redirects']:
+            self.stats['redirects'][service] = 1
 
-	def get_stats(self):
-		return self.stats
+    def get_stats(self):
+        return self.stats
