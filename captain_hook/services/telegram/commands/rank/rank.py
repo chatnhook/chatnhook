0e2fe427523c32c7ae8cc3fb8b61a8e7fd455a16
brantje <brantje@gmail.com>
2017-11-11 23:13:13 +0100

    Implement basic bot permissions

---
+++ captain_hook/services/telegram/commands/rank/rank.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from ..base import BaseCommand
+from time import sleep
+
+
+class RankCommand(BaseCommand):
+    def get_description(self):
+        return "Get information about you permissions"
+
+    def run(self, messageObj, config):
+        username = messageObj.get('from', {}).get('username', '')
+        rank = self.get_rank(username)
+
+        message = 'Hi {username}! You rank is *{rank}*'.format(username=username, rank=rank)
+        self.send_message(
+            chat_id=messageObj.get('chat').get('id'),
+            text=message)
