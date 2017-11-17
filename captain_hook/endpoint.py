ba89e98111102e5ff6f28775f22bdd9f81da447c
brantje <brantje@gmail.com>
2017-11-17 23:25:46 +0100

    Return error 500 instead of 200

--- captain_hook/endpoint.py
+++ captain_hook/endpoint.py
@@ -67,7 +67,7 @@ def internal_server_error(error):
     return render_template('500.html',
                            event_id=g.sentry_event_id,
                            public_dsn=sentry.client.get_public_dsn('https')
-                           )
+                           ), 500
 
 
 @application.route('/<service>', methods=['GET', 'POST'])
