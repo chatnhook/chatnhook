2b7f0ca0569a6a985a00f4816a60abd211997512
brantje <brantje@gmail.com>
2017-11-20 22:47:21 +0100

    Start with Bitbucket service

---
+++ captain_hook/services/bitbucket/__init__.py
@@ -0,0 +1,2 @@
+from __future__ import absolute_import  # NOQA
+from .bitbucket import BitbucketService  # NOQA
---
+++ captain_hook/services/bitbucket/events/__init__.py
@@ -0,0 +1,24 @@
+from ...base.events import BaseEvent
+import requests
+from json import loads
+
+
+class BitbucketEvent(BaseEvent):
+    def bb_api(self, url):
+        if 'https://api.bitbucket.org/2.0/' not in url:
+            url = 'https://api.bitbucket.org/2.0/' + url
+
+        headers = {
+            'User-Agent': 'Hookbot',
+        }
+        if 'token' in self.config:
+            headers['Authorization'] = 'token ' + self.config.get('token')
+
+        response = requests.get(url=url, headers=headers)
+
+        try:
+            result = loads(response.text)
+        except TypeError:
+            result = False
+
+        return result
