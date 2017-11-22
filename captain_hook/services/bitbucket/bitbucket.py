64f6ab452398b585dc5abc642544245ee2586af2
brantje <brantje@gmail.com>
2017-11-22 22:48:21 +0100

    Add @classmethod

--- captain_hook/services/bitbucket/bitbucket.py
+++ captain_hook/services/bitbucket/bitbucket.py
@@ -3,5 +3,6 @@ from ..base import BaseService
 
 
 class BitbucketService(BaseService):
+    @classmethod
     def get_event(self, request, body):
         return request.headers['x-event-key'].replace(':', '_')
