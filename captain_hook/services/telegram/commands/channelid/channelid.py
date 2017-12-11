d388686ff7c821dbfd0d950fa783c28013f114bb
brantje <brantje@gmail.com>
2017-12-11 15:18:11 +0100

    Add channelid command (requires admin priv)

---
+++ captain_hook/services/telegram/commands/channelid/channelid.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from ..base import BaseCommand
+
+
+class ChannelidCommand(BaseCommand):
+    def get_description(self):
+        return "Get the current channel id"
+
+    def run(self, messageObj, config):
+        username = messageObj.get('from', {}).get('username', '')
+
+        if not self.is_admin(username):
+            message = 'You don\'t have access to this command!'
+            self.send_message(
+                chat_id=messageObj.get('chat').get('id'),
+                text=message)
+        else:
+            message = "The channel id is: {channel}" \
+                .format(channel=messageObj.get('chat').get('id'))
+
+        self.send_message(
+            chat_id=messageObj.get('chat').get('id'),
+            text=message)
