62aebf2826e0be9d2f504120440262a0dec5087c
brantje <brantje@gmail.com>
2017-12-20 11:00:28 +0100

    Add telegram services

--- captain_hook/services/telegram/events/message.py
+++ captain_hook/services/telegram/events/message.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 from __future__ import absolute_import
 from ...base.events import BaseEvent
 import threading
 import importlib
 from utils import strings
 import logging
+from pprint import pprint
+import os
 
 log = logging.getLogger('hookbot')
 
@@ -12,38 +15,37 @@ log = logging.getLogger('hookbot')
 class MessageEvent(BaseEvent):
     def process(self, request, body):
         update = body
-        if update.get('message', '') and update.get('message').get('text') == 'ping':
-            return {"telegram": str('Pong!')}
-        else:
-            message = update.get('message', '').get('text')
-            if message and message[0][0] == '/':
-                messageSplit = message[1:].split(' ')
-                if '@' in messageSplit[0]:
-                    messageSplit = messageSplit[0].split('@')
-                command = messageSplit[0]
-                messageSplit.pop(0)
-                update['message']['command'] = command
-                update['message']['args'] = messageSplit
-                if command == 'base':
-                    return {'telegram': ''}
-                try:
-                    command_module = self.process_command(command)
-                    if command_module:
-                        t = threading.Thread(target=command_module.run,
-                                             args=(update.get('message'), self.config))
-                        t.start()
-                except ImportError:
-                    log.warn("Don't know how to handle telegram command {}".format(command))
-                    return {"telegram": str('')}
-            return {"telegram": str('')}
+        self._call_services(update.get('message'))
+
+        message = update.get('message', '').get('text')
+        if message and message[0][0] == '/':
+            messageSplit = message[1:].split(' ')
+            if '@' in messageSplit[0]:
+                messageSplit = messageSplit[0].split('@')
+            command = messageSplit[0]
+            messageSplit.pop(0)
+            update['message']['command'] = command
+            update['message']['args'] = messageSplit
+            if command == 'base':
+                return {'telegram': ''}
+            try:
+                command_module = self.process_command(command)
+                if command_module:
+                    t = threading.Thread(target=command_module.run,
+                                         args=(update.get('message'), self.config))
+                    t.start()
+            except ImportError:
+                log.warn("Don't know how to handle telegram command {}".format(command))
+                return {"telegram": str('')}
+        return {"telegram": str('')}
 
     def process_command(self, command):
         command_module = False
         try:
-            command_module = self._import_command_module(command)
+            command_module = self._import_module('commands','core', command)
         except ImportError:
             try:
-                command_module = self._import_custom_command_module(command)
+                command_module = self._import_module('commands','custom', command)
             except ImportError as e:
                 print(str(e))
                 log.warn("Don't know how to handle telegram command {}".format(command))
@@ -57,16 +59,44 @@ class MessageEvent(BaseEvent):
             )
         return False
 
-    def _import_command_module(self, command):
-        package = "services.telegram.commands.core.{}.{}".format(
+    def _import_module(self, type, which , command):
+        package = "services.telegram.{}.{}.{}.{}".format(
+            type,
+            which,
             command,
             command
         )
         return importlib.import_module(package)
 
-    def _import_custom_command_module(self, command):
-        package = "services.telegram.commands.custom.{}.{}".format(
-            command,
-            command
+    def _call_services(self, message):
+        dir_path = os.path.dirname(os.path.realpath(os.path.dirname(__file__))) +'/services/core'
+        command_list = os.walk(dir_path).next()[1]
+        for service in command_list:
+            if service[0] is not '.':
+                service_module = self.process_service(service)
+                if service_module:
+                    t = threading.Thread(target=service_module.run,
+                                         args=(message, self.config))
+                    t.start()
+
+
+
+    def process_service(self, service):
+        service_module = False
+        try:
+            service_module = self._import_module('services','core', service)
+        except ImportError,e:
+
+            try:
+                service_module = self._import_module('commands','custom', command)
+            except ImportError as e:
+                print(str(e))
+                log.warn("Don't know how to handle telegram command {}".format(command))
+
+        service_processor_class_name = "{}Service".format(
+            strings.toCamelCase(service),
         )
-        return importlib.import_module(package)
+        if service_module:
+            return getattr(service_module, service_processor_class_name)(
+                config=self.config,
+            )
---
+++ captain_hook/services/telegram/services/core/ping/ping.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from ....commands.base import BaseCommand
+from time import sleep
+
+
+class PingService(BaseCommand):
+    def run(self, messageObj, config):
+        if messageObj.get('text', '') == 'ping':
+            self.send_message(
+                chat_id=messageObj.get('chat').get('id'),
+                text='Pong!')
+
