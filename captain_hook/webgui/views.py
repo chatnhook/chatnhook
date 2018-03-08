2101a375252182192526bd9d515dd78cd6c1e3f1
brantje <brantje@gmail.com>
2018-03-08 12:49:26 +0100

    New menu

--- captain_hook/webgui/views.py
+++ captain_hook/webgui/views.py
@@ -36,107 +36,42 @@ class AdminIndexView(admin.AdminIndexView):
             return redirect(url_for('.login_view'))
             
         self._stubs()
-        self.header = "Dashboard"
+        self.header = 'Dashboard'
         return render_template('admin/pages/dashboard.html', admin_view=self)
     
-    @expose('/blank')
-    def blank(self):        
+    @expose('/configuration/comms')
+    def comm_config(self):
         if not login.current_user.is_authenticated:
             return redirect(url_for('.login_view'))
             
         self._stubs()    
-        self.header = "Blank"
+        self.header = 'Comms configuration'
         return render_template('admin/pages/blank.html', admin_view=self)
         
-    @expose('/flot')
-    def flot(self):        
+    @expose('/configuration/services')
+    def service_config(self):
         if not login.current_user.is_authenticated:
             return redirect(url_for('.login_view'))
             
-        self._stubs()    
-        self.header = "Flot Charts"
-        return render_template('admin/pages/flot.html', admin_view=self)
+        self.header = 'Service configuration'
+        return render_template('admin/pages/blank.html', admin_view=self)
 
-    @expose('/morris')
-    def morris(self):        
+    @expose('/configuration/projects')
+    def project_config(self):
         if not login.current_user.is_authenticated:
             return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Morris Charts"
-        return render_template('admin/pages/morris.html', admin_view=self)
-        
-    @expose('/tables')
-    def tables(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Tables"
-        return render_template('admin/pages/tables.html', admin_view=self)
-        
-    @expose('/forms')
-    def forms(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Forms"
-        return render_template('admin/pages/forms.html', admin_view=self)
-        
-    @expose('/ui/panelswells')
-    def panelswells(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Panels Wells"
-        return render_template('admin/pages/ui/panels-wells.html', admin_view=self)
-        
-    @expose('/ui/buttons')
-    def buttons(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Buttons"
-        return render_template('admin/pages/ui/buttons.html', admin_view=self)
-                                
-    @expose('/ui/notifications')
-    def notifications(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Notifications"
-        return render_template('admin/pages/ui/notifications.html', admin_view=self)
 
-    @expose('/ui/typography')
-    def typography(self):        
+        self.header = 'Projects configuration'
+        return render_template('admin/pages/blank.html', admin_view=self)
+
+    @expose('/inspector')
+    def webhook_inspector(self):
         if not login.current_user.is_authenticated:
             return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Typography"
-        return render_template('admin/pages/ui/typography.html', admin_view=self)
-        
-    @expose('/ui/icons')
-    def icons(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Icons"
-        return render_template('admin/pages/ui/icons.html', admin_view=self)
-        
-    @expose('/ui/grid')
-    def grid(self):        
-        if not login.current_user.is_authenticated:
-            return redirect(url_for('.login_view'))
-            
-        self._stubs()    
-        self.header = "Grid"
-        return render_template('admin/pages/ui/grid.html', admin_view=self)
+
+        self.header = 'Webhook inspector'
+        return render_template('admin/pages/blank.html', admin_view=self)
+
 
     @expose('/login/', methods=('GET', 'POST'))
     def login_view(self):
