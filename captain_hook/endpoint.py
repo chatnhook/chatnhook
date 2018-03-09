b818a2bc48688233d71ae3924564c572ea5c819c
brantje <brantje@gmail.com>
2018-03-09 08:57:50 +0100

    Disable sentry when config.global.debug is true

--- captain_hook/endpoint.py
+++ captain_hook/endpoint.py
@@ -21,7 +21,8 @@ bot_stats = BotStats()
 log = setup_logger()
 
 dsn = 'https://a3aa56ba615c4085ae8855ab78e4c021:a0f50be103034d9eb71331378e8f1da2@sentry.io/245538'
-if config.get('global', {}).get('enable_sentry', True):
+isDev = config.get('global', {}).get('debug', False)
+if config.get('global', {}).get('enable_sentry', True) and not isDev:
     sentry = Sentry(application, dsn=dsn)
 
 
