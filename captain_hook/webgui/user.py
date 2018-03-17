35ce2c8a19789961497e1e69346fa2d1016cc800
brantje <brantje@gmail.com>
2018-03-17 19:43:11 +0100

    Add user back for now

---
+++ captain_hook/webgui/user.py
@@ -0,0 +1,26 @@
+from flask_login import UserMixin
+
+class UserNotFoundError(Exception):
+    pass
+
+class User(UserMixin):
+
+    USERS = {
+        # username: password
+    }
+
+    id = None
+    password = None
+
+    def __init__(self, id):
+        if not id in self.USERS:
+            raise UserNotFoundError()
+        self.id = id
+        self.password = self.USERS[id]
+
+    @classmethod
+    def get(self_class, id):
+        try:
+            return self_class(id)
+        except UserNotFoundError:
+            return None
