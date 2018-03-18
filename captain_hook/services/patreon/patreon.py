00463195ad588d2e5988922be8a77ce1e532b1fc
brantje <brantje@gmail.com>
2018-03-18 14:23:59 +0100

    Add verification_key to patreon

--- captain_hook/services/patreon/patreon.py
+++ captain_hook/services/patreon/patreon.py
@@ -1,7 +1,41 @@
 from __future__ import absolute_import
 from ..base import BaseService
-
+from flask import abort
 
 class PatreonService(BaseService):
     def get_event(self, request, body):
+
+        key = self.project_service_config.get('settings', {}).get('verification_key', False)
+        if not key:
+            key = self.config.get('verification_key', False)
+        if key:
+            if key != request.args.get('verification_key', False):
+                return False
+
         return request.headers.get('X-Patreon-Event').replace(':', '_')
+
+    def get_service_config_model(self):
+        desc = 'This is the global verification key, and will be used at all projects.<br />' \
+               'Unless configured on project level. <br />' \
+               'This will be used to protect your endpoints'
+        return [
+            {
+                'name': 'verification_key',
+                'label': 'Verification key',
+                'type': 'text',
+                'description': desc
+            }
+        ]
+
+    def get_service_project_config_model(self):
+        desc = 'This is the global verification key, and will be used at all projects.<br />' \
+               'Unless configured on project level. <br />' \
+               'This will be used to protect your endpoints'
+        return [
+            {
+                'name': 'verification_key',
+                'label': 'Verification key',
+                'type': 'text',
+                'description': desc
+            }
+        ]
