79257f8332be035ca8e375145bf36f16e870508f
brantje <brantje@gmail.com>
2018-03-18 14:26:09 +0100

    Add verification_key to codacy

--- captain_hook/services/codacy/codacy.py
+++ captain_hook/services/codacy/codacy.py
@@ -4,11 +4,39 @@ import os
 
 
 class CodacyService(BaseService):
-    @classmethod
     def get_event(self, request, body):
+
+        key = self.project_service_config.get('settings', {}).get('verification_key', False)
+        if not key:
+            key = self.config.get('verification_key', False)
+        if key:
+            if key != request.args.get('verification_key', False):
+                return False
+
         return 'review_complete'
 
-    def get_config_template(self):
-        dir = os.path.dirname(os.path.realpath(__file__))
-        contents = open(dir + '/service.html').read()
-        return contents
+    def get_service_config_model(self):
+        desc = 'This is the global verification key, and will be used at all projects.<br />' \
+               'Unless configured on project level. <br />' \
+               'This will be used to protect your endpoints'
+        return [
+            {
+                'name': 'verification_key',
+                'label': 'Verification key',
+                'type': 'text',
+                'description': desc
+            }
+        ]
+
+    def get_service_project_config_model(self):
+        desc = 'This is the global verification key, and will be used at all projects.<br />' \
+               'Unless configured on project level. <br />' \
+               'This will be used to protect your endpoints'
+        return [
+            {
+                'name': 'verification_key',
+                'label': 'Verification key',
+                'type': 'text',
+                'description': desc
+            }
+        ]
